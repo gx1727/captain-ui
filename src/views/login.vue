6a4093b7bb4a8af0a690d5eb32fcb1352ea20724
gx1727 <gx1727@163.com>
2018-03-14 14:25:46 +0800

    开发

--- src/views/login.vue
+++ src/views/login.vue
@@ -14,14 +14,14 @@
                     <Form ref="loginForm" :model="form" :rules="rules">
                         <FormItem prop="userName">
                             <Input v-model="form.userName" placeholder="请输入用户名">
-                                <span slot="prepend">
+                            <span slot="prepend">
                                     <Icon :size="16" type="person"></Icon>
                                 </span>
                             </Input>
                         </FormItem>
                         <FormItem prop="password">
                             <Input type="password" v-model="form.password" placeholder="请输入密码">
-                                <span slot="prepend">
+                            <span slot="prepend">
                                     <Icon :size="14" type="locked"></Icon>
                                 </span>
                             </Input>
@@ -38,47 +38,48 @@
 </template>
 
 <script>
-import Cookies from 'js-cookie';
-import captain from '@/libs/captain.js';
+    import Cookies from 'js-cookie';
+    import util from '@/libs/util.js';
 
-export default {
-    data () {
-        return {
-            form: {
-                userName: 'iview_admin',
-                password: ''
-            },
-            rules: {
-                userName: [
-                    { required: true, message: '账号不能为空', trigger: 'blur' }
-                ],
-                password: [
-                    { required: true, message: '密码不能为空', trigger: 'blur' }
-                ]
-            }
-        };
-    },
-    methods: {
-        handleSubmit () {
-            this.$refs.loginForm.validate((valid) => {
-                if (valid) {
-//                    alert(this.form.userName);
-                    Cookies.set('user', this.form.userName);
-                    Cookies.set('password', this.form.password);
-                    this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
-                    if (this.form.userName === 'iview_admin') {
-                        Cookies.set('access', 0);
-                    } else {
-                        Cookies.set('access', 1);
-                    }
-                    this.$router.push({
-                        name: 'home_index'
-                    });
+    export default {
+        data () {
+            return {
+                form: {
+                    userName: 'iview_admin',
+                    password: ''
+                },
+                rules: {
+                    userName: [
+                        {required: true, message: '账号不能为空', trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true, message: '密码不能为空', trigger: 'blur'}
+                    ]
                 }
-            });
+            };
+        },
+        methods: {
+            handleSubmit () {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        console.log(util.secret(this.form.userName));
+                        console.log(util.md5(this.form.userName));
+//                    Cookies.set('user', this.form.userName);
+//                    Cookies.set('password', this.form.password);
+                        this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
+                        if (this.form.userName === 'iview_admin') {
+                            Cookies.set('access', 0);
+                        } else {
+                            Cookies.set('access', 1);
+                        }
+                        this.$router.push({
+                            name: 'home_index'
+                        });
+                    }
+                });
+            }
         }
-    }
-};
+    };
 </script>
 
 <style>
