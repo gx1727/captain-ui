9a4f15e150f805d4f36a1814d0bb0c98224121bd
gx1727 <gx1727@163.com>
2018-03-14 08:38:16 +0800

    开发

--- src/views/login.vue
+++ src/views/login.vue
@@ -62,9 +62,14 @@ export default {
         handleSubmit () {
             this.$refs.loginForm.validate((valid) => {
                 if (valid) {
-//                    alert(this.form.userName);
-                    Cookies.set('user', this.form.userName);
-                    Cookies.set('password', this.form.password);
+                    console.log(captain);
+                    console.log(captain.secret);
+                    alert(this.form.userName);
+                    console.log(captain.secret(this.form.password));
+                    alert(captain.secret(this.form.password));
+
+//                    Cookies.set('user', this.form.userName);
+//                    Cookies.set('password', this.form.password);
                     this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
                     if (this.form.userName === 'iview_admin') {
                         Cookies.set('access', 0);
