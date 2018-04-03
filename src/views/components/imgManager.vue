380c7214d46168b0af8ea4bd6afe09a9f7e9b3c8
gx1727 <gx1727@163.com>
2018-04-03 15:20:44 +0800

    开发

--- src/views/components/imgManager.vue
+++ src/views/components/imgManager.vue
@@ -9,45 +9,125 @@
                 :scrollable="true"
                 :mask-closable="false"
                 :styles="{top: '20px'}">
-            <Row :gutter="16" style="border-bottom: 1px solid #ababab;padding-bottom: 3px;">
-                <Col span="10">
-                <ButtonGroup>
-                    <Button>素材库</Button>
-                    <Button type="primary">本地上传</Button>
-                    <Button>网络图片</Button>
-                </ButtonGroup>
-                </Col>
-                <Col span="4">
-                <ButtonGroup style="float: right;">
-                    <Button type="primary" icon="android-list"></Button>
-                    <Button icon="android-apps"></Button>
-                </ButtonGroup>
-                </Col>
-                <Col span="10">
-                <Input v-model="searchKey" icon="ios-clock-outline" placeholder="输入关系词..."></Input>
-                </Col>
-            </Row>
-            <Tabs >
+            <Tabs>
                 <TabPane label="素材库" style="height: 70vh;overflow: auto;">
                     <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>
-                    <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p> <p>dddddddddddddd</p>sss
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>
+                    <p>dddddddddddddd</p>sss
 
                 </TabPane>
                 <TabPane label="本地上传">标签二的内容</TabPane>
                 <TabPane label="网络图片">标签三的内容</TabPane>
-                <Input v-model="searchKey" icon="ios-clock-outline" placeholder="输入关系词..."></Input>
-                <!--<Button type="ghost" @click="handleTabsAdd" size="small" slot="extra">增加</Button>-->
+                <Row slot="extra" style="width: 45vw;">
+                    <Col span="5">
+                        <Select>
+                            <Option value="all">全部</Option>
+                            <Option value="s">收藏</Option>
+                        </Select>
+                    </Col>
+                    <Col span="5" style="text-align: center;">
+                    <ButtonGroup>
+                        <Button type="primary" icon="android-list"></Button>
+                        <Button icon="android-apps"></Button>
+                    </ButtonGroup>
+                    </Col>
+                    <Col span="14" style="text-align: right;">
+                    <Input v-model="searchKey" icon="ios-clock-outline" placeholder="输入关系词..."></Input>
+                    </Col>
+                </Row>
             </Tabs>
 
         </Modal>
